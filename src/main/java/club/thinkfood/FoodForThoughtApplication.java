1332e6aa6ea230cc59144293607cb0bc94b8f245
Rodriques <rodriques.perry@gmail.com>
2022-01-14 15:27:39 -0600

    initial commit

---
+++ src/main/java/club/thinkfood/FoodForThoughtApplication.java
@@ -0,0 +1,13 @@
+package club.thinkfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodForThoughtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodForThoughtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/thinkfood/FoodForThoughtApplicationTests.java
@@ -0,0 +1,13 @@
+package club.thinkfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodForThoughtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
