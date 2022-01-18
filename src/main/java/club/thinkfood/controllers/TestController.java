5c4c471c7acf4db56cfad71628b59f925de4eaad
Alex Riley <alex.riley.ar12@gmail.com>
2022-01-18 14:04:38 -0600

    have spoonacular api working

---
+++ src/main/java/club/thinkfood/controllers/TestController.java
@@ -0,0 +1,13 @@
+package club.thinkfood.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/index")
+    public String TestRecipe(){
+        return "index";
+    }
+}
