77588021f1d1649ea57d8a06263587220940af1f
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2022-01-21 17:22:34 -0600

    search queris now show up on html

--- src/main/java/club/thinkfood/controllers/HomeController.java
+++ src/main/java/club/thinkfood/controllers/HomeController.java
@@ -1,13 +1,24 @@
 package club.thinkfood.controllers;
 
 
+import club.thinkfood.models.Image;
+import club.thinkfood.models.Recipe;
+import club.thinkfood.repositories.RecipeRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+
+    private final RecipeRepository recipeDao;
+
+    public HomeController(RecipeRepository recipeDao){
+        this.recipeDao = recipeDao;
+    }
 
     @GetMapping("/")
     public String HomePage(){
@@ -20,17 +31,24 @@ public class HomeController {
         return "redirect:/users/sign-up";
     }
 
-    @GetMapping("/recipe-search/{search}")
-    public String recipeSearch(@PathVariable String search, Model model){
-        model.addAttribute("search", search);
+    @GetMapping("/recipe-search/")
+    public String recipeSearch(Model model, @RequestParam(name = "search") String search){
+
+        System.out.println("search = " + search);
+//        List<Recipe> recipes = recipeDao.findAllByTitle(search);
+//        List<Recipe> recipes = recipeDao.findRecipesByTitleLike(search);
+        List<Recipe> recipes = recipeDao.findRecipesByTitleContains(search);
+
+        System.out.println("recipes.get(0).getTitle() = " + recipes.get(0).getTitle());
+        model.addAttribute("recipes", recipes);
         return "recipe-search";
     }
-
-    @PostMapping("/recipe-search/{search}")
-    public String getSearch(@RequestParam(name = "search") String search){
-
-        return "redirect:/recipe-search/" + search;
-    }
+//
+//    @PostMapping("/recipe-search/{search}")
+//    public String getSearch(@PathVariable(name = "search") String search){
+//
+//        return "redirect:/recipe-search/" + search;
+//    }
 
     @GetMapping("/questionnaire")
     public String getQuestionnaire(){
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,9 +1,17 @@
-// const client = filestack.init(filestackKey);
-// client.picker().open();
+const client = filestack.init(filestackKey);
+client.picker().open();
 
 $("#rando").click(function(e) {
     e.preventDefault();
     $.get("https://api.spoonacular.com/recipes/random?apiKey=" + spoonKey).done(function (data, status) {
         console.log(data)
+    })
+})
+
+$("#submitSearch").click(function (e){
+    e.preventDefault();
+    // $.get("https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularApiKey).done(function (data){
+        $.get("https://api.spoonacular.com/recipes/random?apiKey=9a977cea6b7849609902f4f01ac80b0c").done(function (data){
+            console.log(data);
     })
 })
