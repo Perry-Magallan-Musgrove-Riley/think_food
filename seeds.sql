844a8a2096858e018eae5300ab83e0e5214b03e8
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2022-01-24 09:33:49 -0600

    configured seeder file

--- seeds.sql
+++ seeds.sql
@@ -1,8 +1,9 @@
-
+DROP DATABASE IF EXISTS think_food_db;
+CREATE DATABASE IF NOT EXISTS think_food_db;
 USE think_food_db;
 
 
-INSERT INTO  recipes(id, title, prep_time,description, rating)
+INSERT INTO  recipes(id, title, prep_time, description, rating)
 VALUES (1, 'Sweet potato casserole', 90, 'some words that make it sound delicious', 8);
 
 INSERT INTO  recipes(id, title, prep_time, description, rating)
