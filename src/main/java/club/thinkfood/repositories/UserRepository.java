cfd5b58c0e252c719d7f3daa891550e68df2002a
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2022-01-24 11:23:57 -0600

    working on description search

--- src/main/java/club/thinkfood/repositories/UserRepository.java
+++ src/main/java/club/thinkfood/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package club.thinkfood.repositories;
 
+import club.thinkfood.models.Recipe;
 import club.thinkfood.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, String> {
     User findUserById(long id);
+    List<Recipe> findUserByUsernameIsLike(User chef);
 }
