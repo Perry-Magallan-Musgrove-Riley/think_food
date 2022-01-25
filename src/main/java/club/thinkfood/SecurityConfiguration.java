aeb6cb4a8a20af25d403dc1bc0e3dce6ee85f3ac
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2022-01-25 12:15:00 -0600

    commented all changes I made on previous push

--- src/main/java/club/thinkfood/SecurityConfiguration.java
+++ src/main/java/club/thinkfood/SecurityConfiguration.java
@@ -1,68 +1,68 @@
-package club.thinkfood;
-
-import club.thinkfood.services.UserDetailsLoader;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private UserDetailsLoader usersLoader;
-
-    public SecurityConfiguration(UserDetailsLoader usersLoader) {
-        this.usersLoader = usersLoader;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(usersLoader) // How to find users by their username
-                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
-        ;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                /* Login configuration */
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/profile{name}") // user's home page, it can be any URL
-                .permitAll() // Anyone can go to the login page
-                /* Logout configuration */
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login?logout") // append a query string value
-                /* Pages that can be viewed without having to log in */
-                .and()
-                .authorizeRequests()
-                .antMatchers("/",
-                        "/login",
-                        "/sign-up") // anyone can see the home login and sign-up page
-                .permitAll()
-                /* Pages that require authentication */
-                .and()
-                .authorizeRequests()
-                .antMatchers(
-                        "/create", // only authenticated users can create recipes
-                        "/edit", // only authenticated users can edit there profile info
-                        "/ingredients", // only authenticated users can see ingredients
-                        "/recipes", // only authenticated users can see recipes
-                        "/profile/{name}", // only authenticated users can see profile page
-                        "/order", // only authenticated users can see orders
-                        "/questionnaire" // only authenticated users can see questionnaire
-                )
-                .authenticated()
-        ;
-    }
-}
+//package club.thinkfood;
+//
+//import club.thinkfood.services.UserDetailsLoader;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    private UserDetailsLoader usersLoader;
+//
+//    public SecurityConfiguration(UserDetailsLoader usersLoader) {
+//        this.usersLoader = usersLoader;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .userDetailsService(usersLoader) // How to find users by their username
+//                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
+//        ;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                /* Login configuration */
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/profile{name}") // user's home page, it can be any URL
+//                .permitAll() // Anyone can go to the login page
+//                /* Logout configuration */
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/login?logout") // append a query string value
+//                /* Pages that can be viewed without having to log in */
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/",
+//                        "/login",
+//                        "/sign-up") // anyone can see the home login and sign-up page
+//                .permitAll()
+//                /* Pages that require authentication */
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers(
+//                        "/create", // only authenticated users can create recipes
+//                        "/edit", // only authenticated users can edit there profile info
+//                        "/ingredients", // only authenticated users can see ingredients
+//                        "/recipes", // only authenticated users can see recipes
+//                        "/profile/{name}", // only authenticated users can see profile page
+//                        "/order", // only authenticated users can see orders
+//                        "/questionnaire" // only authenticated users can see questionnaire
+//                )
+//                .authenticated()
+//        ;
+//    }
+//}
--- src/main/java/club/thinkfood/services/UserDetailsLoader.java
+++ src/main/java/club/thinkfood/services/UserDetailsLoader.java
@@ -1,28 +1,28 @@
-package club.thinkfood.services;
-
-import club.thinkfood.models.User;
-import club.thinkfood.repositories.UserRepository;
-import club.thinkfood.repositories.UserWithRoles;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserDetailsLoader implements UserDetailsService {
-    private final UserRepository users;
-
-    public UserDetailsLoader(UserRepository users) {
-        this.users = users;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = users.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("No user found for " + username);
-        }
-
-        return new UserWithRoles(user);
-    }
-}
+//package club.thinkfood.services;
+//
+//import club.thinkfood.models.User;
+//import club.thinkfood.repositories.UserRepository;
+//import club.thinkfood.repositories.UserWithRoles;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UserDetailsLoader implements UserDetailsService {
+//    private final UserRepository users;
+//
+//    public UserDetailsLoader(UserRepository users) {
+//        this.users = users;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = users.findByUsername(username);
+//        if (user == null) {
+//            throw new UsernameNotFoundException("No user found for " + username);
+//        }
+//
+//        return new UserWithRoles(user);
+//    }
+//}
