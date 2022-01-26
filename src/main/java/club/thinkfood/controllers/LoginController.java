a590d04ef9167966beea9032634a7e35befd8577
Rodriques <rodriques.perry@gmail.com>
2022-01-26 12:53:59 -0600

    refactor: add categories.js

--- src/main/java/club/thinkfood/controllers/LoginController.java
+++ src/main/java/club/thinkfood/controllers/LoginController.java
@@ -1,13 +1,10 @@
 package club.thinkfood.controllers;
 
-import club.thinkfood.models.User;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class LoginController {
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,12 +1,24 @@
-/*<![CDATA[*/
-
-const spoonkey = /*[[${spoonkey}]]*/ 'default';
-$("#submitSearch").click(function (e){
-    e.preventDefault();
-    $.get("https://api.spoonacular.com/recipes/random?apiKey=" + spoonkey).done(function (data){
-            console.log(data);
+"use strict";
+(function () {
+    //Search Functionality
+    $("#submitSearch").click(function (e){
+        e.preventDefault();
+        $.get("https://api.spoonacular.com/recipes/random?apiKey=" + spoonkey).done(function (data){
+                console.log(data);
+        })
     })
-})
+
+    //Randomize Recipe
+    const url = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonkey;
+    const options = {
+        method: "GET"
+    };
+    const spoon = function(e) {
+        e.preventDefault();
+        fetch(url, options).then(response => response.json())
+            .then(response => console.log(response.recipes[0]))
+    }
+    document.getElementById("rando").addEventListener('click', spoon);
 
 
-/*]]>*/
+})();
