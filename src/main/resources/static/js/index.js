39f06e4ca12edc72094af98ba42c34e24aeb789b
Rodriques <rodriques.perry@gmail.com>
2022-01-26 12:20:54 -0600

    refactor: HomeController and index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,12 @@
-// const client = filestack.init(filestackKey);
-// client.picker().open();
+/*<![CDATA[*/
 
-
+const spoonkey = /*[[${spoonkey}]]*/ 'default';
 $("#submitSearch").click(function (e){
     e.preventDefault();
-    $.get("https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularApiKey).done(function (data){
+    $.get("https://api.spoonacular.com/recipes/random?apiKey=" + spoonkey).done(function (data){
             console.log(data);
     })
 })
+
+
+/*]]>*/
