f8a7843807ed318b7b6388ee2257dc6f8ac3db9e
Alex Riley <alex.riley.ar12@gmail.com>
2022-01-27 08:53:03 -0600

    added bad imports back for merge issue

--- src/main/java/club/thinkfood/controllers/AdminController.java
+++ src/main/java/club/thinkfood/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package club.thinkfood.controllers;
 
-
+import club.thinkfood.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
