3ee4e56b76e5ece4ef7e02de060b4590b1da5853
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2022-02-03 11:57:05 -0600

    Have admin roles specified and recipes edit, delete buttons only show for admin

--- src/main/java/club/thinkfood/controllers/RecipesController.java
+++ src/main/java/club/thinkfood/controllers/RecipesController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -33,6 +34,10 @@ public class RecipesController {
         List<Recipe> recipes = recipeDao.findAll();
 
         model.addAttribute("recipes", recipes);
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User currentUser = userDao.findUserById(id);
+        model.addAttribute("user", user);
+        System.out.println("user.getIsAdmin() = " + user.getIsAdmin());
 
         return "users/recipe";
     }
