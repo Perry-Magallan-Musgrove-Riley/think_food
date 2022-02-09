988b3490183033c7bbcf36a703240c5fcc4a8496
Rodriques <rodriques.perry@gmail.com>
2022-02-09 11:35:45 -0600

    error tracking

--- src/main/java/club/thinkfood/controllers/RecipesController.java
+++ src/main/java/club/thinkfood/controllers/RecipesController.java
@@ -39,6 +39,7 @@ public class RecipesController {
 
         model.addAttribute("recipes", recipes);
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("user.getUsername() = " + user.getUsername());
 //        User currentUser = userDao.findUserById(id);
         model.addAttribute("user", user);
         System.out.println("user.getIsAdmin() = " + user.getIsAdmin());
